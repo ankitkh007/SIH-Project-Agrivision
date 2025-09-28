1c059bf4eb5790bb860d47f99743cccd3a96077b
ankitkh007 <ankit123khanna456@gmail.com>
2025-09-28 18:59:35 +0530

    Initial commit

--- backend/build.sh
+++ backend/build.sh
@@ -4,7 +4,7 @@
 python -m pip install --upgrade pip setuptools wheel --force-reinstall --no-cache-dir
 
 # Install dependencies from your Requirement file
-pip install --no-cache-dir -r Requirement
+pip install --no-cache-dir -r requirement.txt
 
 # Optional: run any migrations or setup here if needed
 # Example: python bend.py  # Only if needed
