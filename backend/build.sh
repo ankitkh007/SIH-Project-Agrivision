198437b227d96b8386dcccff0dd756e9297053bc
ankitkh007 <ankit123khanna456@gmail.com>
2025-09-28 19:35:04 +0530

    Initial commit

--- backend/build.sh
+++ backend/build.sh
@@ -7,7 +7,7 @@ python -m pip install --upgrade pip setuptools wheel --force-reinstall --no-cach
 pip install --no-cache-dir Cython==0.29.36
 
 # 3️⃣ Install rest of the dependencies
-pip install --no-cache-dir -r requirement.txt
+pip install --no-cache-dir -r requirements.txt
 
 # ✅ Optional: migrations or pre-start commands
 # python bend.py  # only if needed
