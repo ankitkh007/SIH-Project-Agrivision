ac98c7932ac4800a90560dbe820dbf319458e560
ankitkh007 <ankit123khanna456@gmail.com>
2025-09-30 18:34:16 +0530

    Initial commit

--- backend/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Upgrade pip & setuptools
-python -m pip install --upgrade pip setuptools wheel
-
-# Install dependencies (force reinstall to make uvicorn available)
-pip install --no-cache-dir --force-reinstall -r requirements.txt
